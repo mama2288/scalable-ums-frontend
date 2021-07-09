4138bdde2fbf6f515f365370028aa5426102d925
gigadot <gigadot@gmail.com>
2021-07-09 22:26:50 +0700

    Configure frontend proxy to backend api

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+  devServer: {
+    host: "localhost", // frontend hostname or ip
+    disableHostCheck: true,
+    port: 8080, // frontend port
+    https: false, // no ssl
+    proxy: {
+      "/api": {
+        // proxy everything from frontend http://localhost:8080/api/** to backend at http://localhost:8081/api/**
+        // that is why all api path on backend should begin with /api
+        target: "http://localhost:8081",
+      },
+    },
+    headers: {
+      // typical header settings
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+    },
+  },
 };
