802d8a9913c47bcdb34e8917c7e2969f2ad8b4ac
gigadot <gigadot@gmail.com>
2021-07-09 22:39:04 +0700

    Finished up the router setup with redirection to login page if not authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
     path: "/about",
@@ -17,7 +18,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
     path: "/login",
@@ -26,12 +27,29 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
-  },
+      import(/* webpackChunkName: "about" */ "../views/Login.vue")
+  }
 ];
 
 const router = new VueRouter({
-  routes,
+  routes
+});
+
+// Setup beforeEach hook to check the logged in sync the loggin states with backend
+router.beforeEach(async (to, from, next) => {
+  // get login state using whomai and axios
+  let response = await Vue.axios.get("/api/whoami");
+  // response.data is our payload
+  store.dispatch("setLoggedInUser", response.data);
+  let isLoggedIn = store.state.isLoggedIn;
+  // if the name of the router is not Login, it needs authorization to access the page
+  if (to.name !== "Login" && !isLoggedIn) {
+    // redirect to login page
+    next({ name: "Login" });
+  } else {
+    // otherwise, let go
+    next();
+  }
 });
 
 export default router;
